4a1a51499db90f0af60928657c10094496568411
Vyacheslav Gorbov <vyacheslav93@gmail.com>
2021-07-26 18:35:39 -0400

    Changed user_search method to convert json data to <list> and added searches table. Also added trim_db func in db_manager.py

--- storage/db_manager.py
+++ storage/db_manager.py
@@ -17,17 +17,36 @@ class DatabaseManager():
                 id integer PRIMARY KEY,
                 name text NOT NULL,
                 country text
-                )"""
+                );"""
+        )
+
+        cursor.execute(
+            """
+            CREATE TABLE IF NOT EXISTS searches (
+                search_id integer PRIMARY KEY,
+                keywords text,
+                
+                );
+            """
         )
 
         cursor.execute("""CREATE TABLE IF NOT EXISTS job_postings (
             id integer PRIMARY KEY,
+            parent_search_id integer NOT NULL,
             title text,
             company text,
             location text,
             post_date text,
             latitude text,
             longitude text,
-            url text
-            )""")
+            url text,
+            category text
+            
+            FOREIGN KEY(parent_search_id) REFERENCES searches(search_id)
+            );""")
+
+        pass
+    
+    def trim_db(self):
+        """Used for chronologically trimming DB size to prevent overuse"""
         pass
--- workers/scraper.py
+++ workers/scraper.py
@@ -2,10 +2,9 @@ from datetime import datetime
 import requests
 import json
 import os
-from dotenv import load_dotenv
 from storage.db_manager import DatabaseManager
 from bs4 import BeautifulSoup as bsoup
-
+from dotenv import load_dotenv
 
 class JobScraper():
     _base_url = "http://api.adzuna.com/v1/api/jobs/{country}/search/1?" \
@@ -19,9 +18,11 @@ class JobScraper():
         load_dotenv()
         self.app_id = os.getenv('ADZUNA_APP_ID')
         self.app_key = os.getenv('ADZUNA_APP_KEY')
-        dbmanager = DatabaseManager()
 
-    def search(self, country, results_qty, query):
+    def user_search(self, country, results_qty, query):
+        """
+        User initiated API search will return the results based on their query.
+        """
         response = requests.get(self._base_url.format(
             country=country,
             app_id=self.app_id,
@@ -30,6 +31,30 @@ class JobScraper():
             query=query
         ))
         
-        obj = json.loads(response.text)
-        print(obj["results"][0]["title"])
-        print(obj["results"][0]["location"]["display_name"])
+        results = json.loads(response.text)["results"]
+        job_posts = []
+        
+        # Convert data from JSON to <list>
+        for result in results:
+            job_post = {
+                "ID": result["id"],
+                "Title": result["title"],
+                "Company": result["company"]["display_name"],
+                "Location": result["location"]["display_name"],
+                "Post Date": result["created"],
+                "Latitude": result["latitude"],
+                "Longitude": result["longitude"],
+                "URL": result["redirect_url"],
+                "Category": result["category"]["label"]
+            }
+            job_posts.append(job_post)
+            
+        return job_posts
+
+        
+    def __cron_search(self):
+        """
+        Application-iniated search which will fill the database preemptively to minimize API calls.
+        """
+        return None
+        
