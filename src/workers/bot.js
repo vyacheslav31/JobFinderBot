d548c678c51874239f75a1080380a4bfb1fbb33f
Vyacheslav Gorbov <slava@vygor.onmicrosoft.com>
2021-10-26 18:46:44 -0400

    Removing the register commands method because commands will be deployed via 'deploy-commands.js'

--- src/workers/bot.js
+++ src/workers/bot.js
@@ -18,7 +18,6 @@ class JobFinderBot extends Client {
         env.config();
         this.commands = [];
         this.requestManager = new RequestManager();
-        this.registerCommands();
     }
 
     init() {
@@ -30,52 +29,11 @@ class JobFinderBot extends Client {
         this.on('ready', () => {
             console.log(`${this.user.tag} is now active!`);
         });
-        this.login(process.env.TOKEN);
+        this.login(process.env.DISCORD_TOKEN);
     }
 
     formatPost(post) {
         // TODO: Implement format post method with discord embeds
-    }
-
-    registerCommands() {
-        var commandFiles = fs.readdirSync("./src/commands").filter(file => file.endsWith('.js'));
-
-        for(const file of commandFiles) {
-            const command = require(`../commands/${file}`);
-            this.commands.push(command.data);
-        }
-
-        const rest = new REST({ version: '9'}).setToken(process.env.TOKEN);
-        (async () => {
-            try {
-              console.log('Started refreshing application (/) commands.');
-          
-              await rest.put(
-                Routes.applicationGuildCommands(process.env.DISCORD_APP_ID, process.env.CHANNEL),
-                { body: this.commands },
-              );
-              
-              console.log('Successfully reloaded application (/) commands.');
-            } catch (error) {
-              console.error(error);
-            }
-        })();
-
-        this.on('interactionCreate', async interaction => {
-            if (!interaction.isCommand()) {
-                return;
-            }
-
-            const command = this.commands.get(interaction.commandName);
-            if (!command) return;
-
-            try {
-                await command.execute(interaction);
-            } catch (error) {
-                console.error(error);
-                return interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
-            }
-        });
     }
 }
 
