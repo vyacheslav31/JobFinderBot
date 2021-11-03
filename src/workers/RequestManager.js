c02398b229f26e966e2c5efb8d727cec34d14d08
Vyacheslav Gorbov <slava@vygor.onmicrosoft.com>
2021-11-03 17:37:46 -0400

    working on saving posts and searches properly after retrieving them

--- src/workers/RequestManager.js
+++ src/workers/RequestManager.js
@@ -8,17 +8,8 @@ class RequestManager {
     }
 
     async requestPosts(userId, query, location, quantity) {
-        // logic for submitting requests
-
-        /**
-         * If db_manager can find the post in the db return post
-         * else request a new post from the scraper and save it in the db, then return it.
-         */
         const region = this.getUserRegion(userId);
-        console.log(region, query, location, quantity);
         const posts = await this.scraper.makeRequest(region, query, location, quantity);
-        console.log(posts);
-        await this.savePosts(posts, query);
         return posts;
 
     }
@@ -31,8 +22,8 @@ class RequestManager {
         return this.dbManager.userExists(userId);
     }
 
-    async savePosts(posts, query) {
-        await this.dbManager.insertPosts(posts, query);
+    savePosts(posts, query) {
+        this.dbManager.insertPosts(posts, query);
     }
 
     getUserRegion(userId) {
